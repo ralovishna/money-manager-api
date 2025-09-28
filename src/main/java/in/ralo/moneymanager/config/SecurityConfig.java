d17adfedd52fe4760e2ed1e82659cc77ebd5ca2b
ralovishna <120235812+ralovishna@users.noreply.github.com>
2025-09-28 16:31:33 +0530

    update security files for frontend

--- src/main/java/in/ralo/moneymanager/config/SecurityConfig.java
+++ src/main/java/in/ralo/moneymanager/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:5173","https://ralomoney.netlify.app"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-type", "Accept"));
         corsConfiguration.setAllowCredentials(true);
